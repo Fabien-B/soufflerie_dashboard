be9def482892b34c10003fb3417f08bebd72d7fb
Fabien-B <Fabien-B@github.com>
2024-11-21 20:42:46 +0100

    [USS] Add PWE AK definitions

--- firmware/source/USS.h
+++ firmware/source/USS.h
@@ -41,6 +41,55 @@ typedef struct {
 } USSConfig;
 
 
+typedef enum {
+    USS_PWE_AK_TASK_NO_TASK,                   // No task
+    USS_PWE_AK_TASK_REQUEST_PWE,               // Request PWE
+    USS_PWE_AK_TASK_CHANGE_PWE_W,              // Change PWE (word)
+    USS_PWE_AK_TASK_CHANGE_PWE_DW,             // Change PWE (double word)
+    USS_PWE_AK_TASK_REQUEST_PBE,               // Request PBE element
+    USS_PWE_AK_TASK_CHANGE_PBE,                // Change PBE element
+    USS_PWE_AK_TASK_REQUEST_PWE_ARRAY,         // Request PWE (array)
+    USS_PWE_AK_TASK_CHANGE_PWE_ARRAY_W,        // Change PWE (array word)
+    USS_PWE_AK_TASK_CHANGE_PWE_ARRAY_DW,       // Change PWE (array double word) 
+    USS_PWE_AK_TASK_REQUEST_NB_ARRAY_ELT,      // Request the number of array elements
+    USS_PWE_AK_TASK_RESERVED,                  // Reserve
+    USS_PWE_AK_TASK_CHANGE_PWE_ARRAY_DW_STR,   // Change PWE (array double word), and store in the EEPROM 
+    USS_PWE_AK_TASK_CHANGE_PWE_ARRAY_W_STR,    // Change PWE (array word), and store in the EEPROM
+    USS_PWE_AK_TASK_CHANGE_PWE_DW_STR,         // Change PWE (double word), and store in EEPROM
+    USS_PWE_AK_TASK_CHANGE_PWE_W_STR,          // Change PWE (word), and store in the EEPROM
+    USS_PWE_AK_TASK_REQUEST_CHANGE_TEXT,       // Request or change text ( only valid for SIMOVERT Master Drives)
+} pwe_ak_task_t;
+
+typedef enum {
+    USS_PWE_AK_RSP_NO_RESPONSE,                    // No response
+    USS_PWE_AK_RSP_TRANSFER_PWE_W,                 // Transfer PWE (word)
+    USS_PWE_AK_RSP_TRANSFER_PWE_DW,                // Transfer PWE (double word)
+    USS_PWE_AK_RSP_TRANSFER_PBE_ELT,               // Transfer the number of array elements
+    USS_PWE_AK_RSP_TRANSFER_PWE_ARRAY_W,           // Transfer PWE (array word)
+    USS_PWE_AK_RSP_TRANSFER_PWE_ARRAY_DW,          // Transfer PWE (array double word)
+    USS_PWE_AK_RSP_TRANSFER_NB_ARRAY_ELT,          // Transfer number of array elements
+    USS_PWE_AK_RSP_TASK_CANNOT_EXEC,               // Task cannot be executed
+    USS_PWE_AK_RSP_PKW_READ_ONLY,                  // No PKW parameter change rights
+    USS_PWE_AK_RSP_PARAM_CHANGE_REPORT_W,          // Parameter change report (word)
+    USS_PWE_AK_RSP_PARAM_CHANGE_REPORT_DW,         // Parameter change report (double word)
+    USS_PWE_AK_RSP_PARAM_CHANGE_REPORT_ARRAY_W,    // Parameter change report (array word)
+    USS_PWE_AK_RSP_PARAM_CHANGE_REPORT_ARRAY_DW,  // Parameter change report (array double word)
+    USS_PWE_AK_RSP_RESERVED,                       // Reserve
+    USS_PWE_AK_RSP_RESERVED,                       // Reserve
+    USS_PWE_AK_RSP_TRANSFER_TEXT,                  // Transfer text
+} pwe_ak_rps_t;
+
+typedef struct {
+    //PKE
+    uint16_t pke_ak : 4;
+    uint16_t pke_sp : 1;
+    uint16_t pke_pnu : 11;
+    //IND
+    uint16_t ind_dcs:6;
+    uint16_t ind_rxte: 2;
+    uint16_t ind_low: 8;
+} pkw_t;
+
 typedef struct {
     uint8_t stx;
     uint8_t lge;
